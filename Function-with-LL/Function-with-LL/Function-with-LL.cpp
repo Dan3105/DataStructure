3b3215135405f8d47d78b04de803c37986e99ef8
dhuynguyen2002 <dhuynguyen2002@users.noreply.github.com>
2022-01-17 13:44:37 +0700

    123

--- Function-with-LL/Function-with-LL/Function-with-LL.cpp
+++ Function-with-LL/Function-with-LL/Function-with-LL.cpp
@@ -9,6 +9,7 @@ bool cmp(int data1, int data2)
 
 struct abc
 {
+struct abc {
 	int a;
 	string b;
 };
@@ -19,6 +20,8 @@ void cr(DLList<int> &m, int pos)
 	{
 		return;
 	};
+void cr(DLList<int> &m, int pos) {
+	if(!m.remove_at(pos)) { return; };
 	cout << "After remove at " << pos << " :";
 	PrintDLL(m);
 	cout << endl;
@@ -30,6 +33,8 @@ void cr(DLList<int> &m, Node<int> *node)
 	{
 		return;
 	}
+void cr(DLList<int> &m, Node<int> *node) {
+	if(!m.remove_node(node)) { return; }
 	cout << "After remove Node: ";
 	PrintDLL(m);
 	cout << endl;
@@ -55,23 +60,31 @@ void cfa(DLList<int> &m, int e)
 
 void test_delete(DLList<int> &m)
 {
+void test_delete(DLList<int> &m) {
 	cout << "Before remove:";
 	PrintDLL(m);
 	cout << endl;
 	Node<int> *temp = m.head;
+	Node<int> *temp = m.head; 
 	cr(m, temp->next->next);
 	temp = m.head;
+	cr(m, temp);
 	cr(m, 4);
 	cr(m, 0);
 	cr(m, m.getLen() - 1);
+	cr(m, m.size()-1);
 	cr(m, 1);
 	cr(m, 3);
 	cr(m, -1);
 	cr(m, m.tail);
 	cr(m, m.head);
 	cr(m, 1);
+	cr(m,m.tail);
+	cr(m,m.head);
+	cr(m,1);
 }
 
+int main()
 {
 	// I'm here
 	// Something change
@@ -83,8 +96,10 @@ void test_delete(DLList<int> &m)
 	m.push_back(1);
 	m.insert_order(4, cmp);
 	m.insert_at(6, 7);
+	m.insert_at(6,7);
 	//Node<int>* temp = m.head;
 	//	MergeSort(temp, 5,cmp);
+//	MergeSort(temp, 5,cmp);
 	//test_delete(m);
 	PrintDLL(m);
 	m.head = mergeSort(m.head, cmp);
@@ -102,4 +117,5 @@ void test_delete(DLList<int> &m)
 	test_delete(m);
 	cout << endl
 			 << m.getLen();
+	cout << endl << m.size();
 }
--- Function-with-LL/Function-with-LL/LinkedList.cpp
+++ Function-with-LL/Function-with-LL/LinkedList.cpp
@@ -128,307 +128,196 @@ public:
 
 				while (temp != NULL)
 				{
-
-					if (cmp(temp->data, data))
-					{
-						if (temp == head)
-						{
-							push_front(data);
-							return 1;
-						}
-						insert(data, temp);
-						return 1;
-					}
-					if (temp == tail)
-					{
-						push_back(data);
-						append(data);
-						return 1;
-					}
-					temp = temp->next;
-				}
-
-				return 0;
 			}
+			temp = temp->next;
+		}
 
-			//Delete
-			int pop_front()
+		return 0;
+	}
+	
+
+	//Delete
+	int pop_front() {
+		if(len==0) {
+			std::cout << "List is empty" << std::endl;
+			return 0;
+		}
+		if(head == tail ) {
+			delete head;
+			head = tail = NULL;
+			len--;
+			return 1;
+		}
+		Node<T>* temp = head;
+		head = head->next;
+		head -> prev = NULL;
+		delete temp;
+		
+		len--;
+		return 1;
+	}
+	
+	int pop_back() {
+		if(len==0) {
+			std::cout << "List is empty" << std::endl;
+			return 0;
+		}
+		if(head == tail ) {
+			delete head;
+			head = tail = NULL;
+			len--;
+			return 1;
+		}
+		Node<T>* temp = tail;
+		tail = tail->prev;
+		tail -> next = NULL;
+		delete temp;
+		
+		len--;
+		
+		return 1;
+	}
+	
+	int remove_node(Node<T>* node) {
+		if(node == NULL) { 
+			std::cout << "Node does not exist" << std::endl;
+			return 0; 
+		}
+		if(node == head) { pop_front(); return 1; };
+		if(node == tail) { pop_back(); return 1; };
+
+		
+		node->prev->next = node ->next;
+		node->next->prev = node ->prev;
+		
+		delete node;
+				
+		len--;
+		return 1;
+		
+	}
+	
+	int remove_at(int pos) {
+		if(len==0) {
+			std::cout << "List is empty" << std::endl;
+			return 0;
+		}
+		if(pos<0 || pos >=len) {
+			std::cout << "Out of range" << std::endl;
+			return 0;
+		}
+		if(pos==0) { pop_front(); return 1; }
+		if(pos==len-1) { pop_back(); return 1; }
+		Node<T>* temp;
+		if(pos<=len/2) {
+			temp = head;
+			for(int i=1; i<=pos; ++i) {
+				temp=temp->next;
+			}
+		} else {
+			temp = tail;
+			for(int i=len-1; i>pos; --i) {
+				temp=temp->prev;
+			}
+		}	
+		len--;
+		if (remove_node(temp)==1) {return 1;}
+		return 0;
+
+	}
+	
+	int size() { return len; }
+	
+		Node<T> *find_node(T element)
+	{
+		if (len == 0)
+		{
+			std::cout << "List is empty" << std::endl;
+			return NULL;
+		}
+		Node<T> *curr =head;
+		while (curr != NULL)
+		{
+			if (curr->data == element)
 			{
-				if (len == 0)
+				return curr;
+			}
+			curr = curr->next;
+		}
+		std::cout << "Node does not exist" << std::endl;
+		return NULL;
+	}
+	
+	DLList<T> find_all(T element)
+	{
+		DLList<T> res(0);
+		res.head = res.tail = NULL;
+		res.len = 0;
+		if (len == 0)
+		{
+			std::cout << "List is empty" << std::endl;
+			return res;
+		}
+	
+		Node<T> *curr = head;
+		while (curr != NULL)
+		{
+			if (curr->data == element)
+			{
+				if (res.head == NULL)
 				{
-					std::cout << "List is empty" << std::endl;
-					return 0;
+					Node<T> *temp = new Node<T>(curr->data);
+					res.head = res.tail = temp;
+					res.len = 1;
 				}
-
-				if (head == tail)
+				else
 				{
+					res.push_back(curr->data);
+				}
+			}
+			curr = curr->next;
+		}
+		if (res.len == 0)
+		{
+			std::cout << "Nodes does not exist" << std::endl;
+		}
+		return res;
+	}
 
-					int pop_front()
-					{
-						if (len == 0)
-						{
-							std::cout << "List is empty" << std::endl;
-							return 0;
-						}
-						if (head == tail)
-						{
+};
 
-							delete head;
-							head = tail = NULL;
-							len--;
-							return 1;
-						}
+template <typename T>
+inline void PrintDLL(DLList<T>& list)
+{
+	Node<T>* temp = list.head;
+	while (temp != NULL)
+	{
+		std::cout << temp->data << " ";
+		temp = temp->next;
+	}
+}
 
-						Node<T> *temp = head;
-						head = head->next;
-						head->prev = NULL;
-						delete temp;
+//template <typename T>
+//inline Node<T>* midNode(Node<T>* head)
+//{
+//	Node<T>* tortoise = head;
+//	Node<T>* hare = head->next;
+//
+//	while ((tortoise->next != NULL) && (hare != NULL && hare->next != NULL))
+//	{
+//		tortoise = tortoise->next;
+//		hare = hare->next->next;
+//	}
+//
+//	return tortoise;
+//}
 
-						len--;
-						return 1;
-					}
-
-					int pop_back()
-					{
-
-						if (len == 0)
-						{
-							std::cout << "List is empty" << std::endl;
-							return 0;
-						}
-
-						if (head == tail)
-						{
-							if (len == 0)
-							{
-								std::cout << "List is empty" << std::endl;
-								return 0;
-							}
-							if (head == tail)
-							{
-								delete head;
-								head = tail = NULL;
-								len--;
-								return 1;
-							}
-							Node<T> *temp = tail;
-							tail = tail->prev;
-							tail->next = NULL;
-							delete temp;
-
-							len--;
-
-							return 1;
-						}
-
-						int remove_node(Node<T> * node)
-						{
-							if (node == NULL)
-							{
-								int remove_node(Node<T> * node)
-								{
-									if (node == NULL)
-									{
-										std::cout << "Node does not exist" << std::endl;
-										return 0;
-									}
-									if (node == head)
-									{
-										pop_front();
-										return 1;
-									};
-									if (node == tail)
-									{
-										pop_back();
-										return 1;
-									};
-
-									node->prev->next = node->next;
-									node->next->prev = node->prev;
-
-									delete node;
-
-									len--;
-									return 1;
-								}
-								int remove_at(int pos)
-								{
-									if (len == 0)
-									{
-										std::cout << "List is empty" << std::endl;
-										return 0;
-									}
-									if (pos < 1 || pos > len)
-									{
-										int remove_at(int pos)
-										{
-											if (len == 0)
-											{
-												std::cout << "List is empty" << std::endl;
-												return 0;
-											}
-											if (pos < 0 || pos >= len)
-											{
-												std::cout << "Out of range" << std::endl;
-												return 0;
-											}
-											if (pos == 0)
-											{
-												pop_front();
-												return 1;
-											}
-											if (pos == len)
-											{
-												pop_back();
-												return 1;
-											}
-											Node<T> *temp;
-
-											if (pos <= len / 2)
-											{
-												temp = head;
-												for (int i = 2; i <= pos; ++i)
-												{
-													temp = temp->next;
-												}
-											}
-											else
-											{
-												temp = tail;
-												for (int i = len; i >= pos; --i)
-												{
-													temp = temp->prev;
-												}
-											}
-											if (remove_node(temp) == 1)
-											{
-												return 1;
-											}
-											if (pos == len - 1)
-											{
-												pop_back();
-												return 1;
-											}
-											Node<T> *temp;
-											if (pos <= len / 2)
-											{
-												temp = head;
-												for (int i = 1; i <= pos; ++i)
-												{
-													temp = temp->next;
-												}
-											}
-											else
-											{
-												temp = tail;
-												for (int i = len - 1; i > pos; --i)
-												{
-													temp = temp->prev;
-												}
-											}
-											len--;
-											if (remove_node(temp) == 1)
-											{
-												return 1;
-											}
-											return 0;
-										}
-
-										int size() { return len; }
-
-										//Search
-										Node<T> *find_node(T element)
-										{
-											if (len == 0)
-											{
-												std::cout << "List is empty" << std::endl;
-												return NULL;
-											}
-											Node<T> *curr = head;
-											while (curr != NULL)
-											{
-												if (curr->data == element)
-												{
-													return curr;
-												}
-												curr = curr->next;
-											}
-											std::cout << "Node does not exist" << std::endl;
-											return NULL;
-										}
-
-										DLList<T> find_all(T element)
-										{
-											DLList<T> res(0);
-											res.head = res.tail = NULL;
-											res.len = 0;
-											if (len == 0)
-											{
-												std::cout << "List is empty" << std::endl;
-												return res;
-											}
-
-											Node<T> *curr = head;
-											while (curr != NULL)
-											{
-												if (curr->data == element)
-												{
-													if (res.head == NULL)
-													{
-														Node<T> *temp = new Node<T>(curr->data);
-														res.head = res.tail = temp;
-														res.len = 1;
-													}
-													else
-													{
-														res.push_back(curr->data);
-													}
-												}
-												curr = curr->next;
-											}
-											if (res.len == 0)
-											{
-												std::cout << "Nodes does not exist" << std::endl;
-											}
-											return res;
-										}
-									};
-
-									int getLen() { return len; }
-								};
-
-								template <typename T>
-								inline void PrintDLL(DLList<T> & list)
-								{
-									Node<T> *temp = list.head;
-									while (temp != NULL)
-									{
-										std::cout << temp->data << " ";
-										temp = temp->next;
-									}
-								}
-								//template <typename T>
-								//inline Node<T>* midNode(Node<T>* head)
-								//{
-								//	Node<T>* tortoise = head;
-								//	Node<T>* hare = head->next;
-								//
-								//	while ((tortoise->next != NULL) && (hare != NULL && hare->next != NULL))
-								//	{
-								//		tortoise = tortoise->next;
-								//		hare = hare->next->next;
-								//	}
-								//
-								//	return tortoise;
-								//}
-
-								//singly linked list
-								/*template <typename T>
+//singly linked list
+/*template <typename T>
 inline Node<T>* Merge(Node<T>** head1, Node<T>** head2, bool (*cmp)(T,T))
 {
 	Node<T>* merged = new Node<T>(head1->data);
 	Node<T>* dummy = merged;
-
 	while (head1 != NULL || head2 != NULL)
 	{
 		if (cmp(head1->data, head2->data))
@@ -443,10 +332,8 @@ inline Node<T>* Merge(Node<T>** head1, Node<T>** head2, bool (*cmp)(T,T))
 			head2 = head2->next;
 			dummy = dummy->next;
 		}
-
 		dummy = dummy->next;
 	}
-
 	while (head1 != NULL)
 	{
 		merged->next = head1;
@@ -459,7 +346,6 @@ inline Node<T>* Merge(Node<T>** head1, Node<T>** head2, bool (*cmp)(T,T))
 		head2 = head2->next;
 		merged = merged->next;
 	}
-
 	return merged->next;
 }
 template <typename T>
@@ -469,71 +355,69 @@ inline Node<T>* MergeSort(Node<T>* head,const int len, bool (*cmp)(T,T))
 		return head;
 	Node<T>* middle = new Node<T>(head->data);
 	Node<T>* head2 = new Node<T>(head->data);
-
 	middle = midNode(head);
 	head2 = middle->next;
 	middle->next = NULL;
 	//head2->prev = NULL;
-
 	Node<T>* sorthead = Merge(&MergeSort(head, len, cmp), &MergeSort(head2, len, cmp), cmp);
 	return sorthead;
 }
 */
 
-								//doubly linked list
-								template <typename T>
-								inline Node<T> *split(Node<T> * head)
-								{
-									Node<T> *fast = head, *slow = head;
-									while (fast->next && fast->next->next)
-									{
-										fast = fast->next->next;
-										slow = slow->next;
-									}
-									Node<T> *temp = slow->next;
-									slow->next = NULL;
-									return temp;
-								}
+//doubly linked list
+template <typename T>
+inline Node<T>* split(Node<T>* head)
+{
+	Node<T>* fast = head, * slow = head;
+	while (fast->next && fast->next->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+	Node<T>* temp = slow->next;
+	slow->next = NULL;
+	return temp;
+}
 
-								template <typename T>
-								inline Node<T> *merge(Node<T> * first, Node<T> * second, bool (*cmp)(T, T))
-								{
-									// If first linked list is empty
-									if (!first)
-										return second;
+template <typename T>
+inline Node<T>* merge(Node<T>* first, Node<T>* second, bool (*cmp)(T, T))
+{
+	// If first linked list is empty 
+	if (!first)
+		return second;
 
-									// If second linked list is empty
-									if (!second)
-										return first;
+	// If second linked list is empty 
+	if (!second)
+		return first;
 
-									// Pick the smaller value
-									if (first->data < second->data)
-									{
-										first->next = merge(first->next, second, cmp);
-										first->next->prev = first;
-										first->prev = NULL;
-										return first;
-									}
-									else
-									{
-										second->next = merge(first, second->next, cmp);
-										second->next->prev = second;
-										second->prev = NULL;
-										return second;
-									}
-								}
+	// Pick the smaller value 
+	if (first->data < second->data)
+	{
+		first->next = merge(first->next, second, cmp);
+		first->next->prev = first;
+		first->prev = NULL;
+		return first;
+	}
+	else
+	{
+		second->next = merge(first, second->next, cmp);
+		second->next->prev = second;
+		second->prev = NULL;
+		return second;
+	}
+}
 
-								template <typename T>
-								inline Node<T> *mergeSort(Node<T> * head, bool (*cmp)(T, T))
-								{
-									if (!head || !head->next)
-										return head;
-									Node<T> *second = split(head);
+template <typename T>
+inline Node<T>* mergeSort(Node<T>* head, bool (*cmp)(T, T))
+{
+	if (!head || !head->next)
+		return head;
+	Node<T>* second = split(head);
 
-									// Recur for left and right halves
-									head = mergeSort(head, cmp);
-									second = mergeSort(second, cmp);
+	// Recur for left and right halves 
+	head = mergeSort(head, cmp);
+	second = mergeSort(second, cmp);
 
-									// Merge the two sorted halves
-									return merge(head, second, cmp);
-								}
+	// Merge the two sorted halves 
+	return merge(head, second, cmp);
+}
